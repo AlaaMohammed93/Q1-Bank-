f3d3123c098f88bb8240ec26b38472be8bc4fbe2
AlaaMohammed93 <alaa.93.mohammed@gmail.com>
2015-10-20 22:09:28 +0200

    Update Bank.cpp

--- Bank.cpp
+++ Bank.cpp
@@ -1,5 +1,6 @@
-#include <string>
 #include <iostream>
+#include <cstdlib>
+#include <string>
 using namespace std;
 class Bank
 {
@@ -8,18 +9,12 @@ private:
 	string acc_no;
 	double balance;
 public:
-	Bank(string NM = " ", string AN = " " , double BL = 0.0)
-	{
-		name = NM;
-		acc_no = AN;
-		balance = BL;
-	}
-
-
+	Bank();
+	Bank(string , string , double );
 	string getname();
 	string getacc_no();
 	double getbalance()
-	{ return balance;}
+	{return balance;}
 	void deposite(double);
 	void withdraw(double);
 };
@@ -44,42 +39,90 @@ void Bank :: withdraw(double WD)
 {
 	balance = balance - WD;
 }
+Bank::Bank(string NM = " ", string AN = " ", double BL = 0.0)
+{
+	name = NM;
+	acc_no = AN;
+	balance = BL;
+}
 int main()
 {
-	Bank ahmad ("ahmad" , "20a" , 30.0);
-	cout << "Name is: " << ahmad.getname() << endl;
-	cout << "Account no.: " << ahmad.getacc_no() << endl;
-	cout << "Balance = $ " << ahmad.getbalance() << endl;
-	cout << "Press d for Deposite, press w for Withdraw or press e to Exit\n";
-	string a;
-	cin >> a;
-	if (a == "d")
+	Bank ahmad("Ahmad", "20a", 30.0);
+	cout << "To creat a new account press 1, To access old account press 2" << endl;
+	int x;
+	cin >> x;
+	if (x == 1)
 	{
-		cout << "Enter amount of deposit in dollars...\n";
-		double de;
-		cin >> de;
-		ahmad.deposite(de);
-		cout << "Your new balance = $ " << ahmad.getbalance()<<endl;
+		string f = "21a";
+		string name;
+		double Bla;
+		cout << "Enter account name\n";
+		cin >> name;
+		cout << "Enter balance in dollars\n";
+		cin >> Bla;
+		Bank new_acc(name,f,Bla);
+		cout << "Name is: " << new_acc.getname() << endl;
+		cout << "Account no.: " << new_acc.getacc_no() << endl;
+		cout << "Balance = $ " << new_acc.getbalance() << endl;
+
+		f="22a";
 	}
-	else if (a == "w")
+	else if (x == 2)
 	{
-		cout << "Enter amount of withdraw in dollars...\n";
-		double we;
-		cin >> we;
-		if (ahmad.getbalance() < we)
-			cout << "Sorry you don't have enough balance...\nYour balance is = $"<< ahmad.getbalance()<<endl;
+		cout << "Enter account name\n";
+		string username;
+		cin >> username;
+		if (username == "Ahmad" || username == "ahmad")
+		{
+			cout << "Name is: " << ahmad.getname() << endl;
+			cout << "Account no.: " << ahmad.getacc_no() << endl;
+			cout << "Balance = $ " << ahmad.getbalance() << endl;
+			cout << "Press d for Deposite, press w for Withdraw or press e to Exit\n";
+			string a;
+			cin >> a;
+			if (a == "d")
+			{
+				cout << "Enter amount of deposit in dollars...\n";
+				double de;
+				cin >> de;
+				ahmad.deposite(de);
+				cout << "Your new balance = $ " << ahmad.getbalance() << endl;
+			}
+			else if (a == "w")
+			{
+				cout << "Enter amount of withdraw in dollars...\n";
+				double we;
+				cin >> we;
+				if (ahmad.getbalance() < we)
+					cout << "Sorry you don't have enough balance...\nYour balance is = $" << ahmad.getbalance() << endl;
+				else
+				{
+					ahmad.withdraw(we);
+					cout << "Your new balance = $ " << ahmad.getbalance() << endl;
+				}
+			}
+			else if (a == "e")
+			{
+				cout << "Good bye!" << endl;
+			}
+			else
+				cout << "Invalid letter" << endl;
+		}
 		else
 		{
-			ahmad.withdraw(we);
-			cout << "Your new balance = $ " << ahmad.getbalance() << endl;
+			cout << "Invalid account\n";
+			exit;
 		}
 	}
-	else if (a == "e")
+	
+	else
 	{
-		cout << "Good bye!"<<endl;
+		cout << "Error\n";
+		exit;
 	}
-	else 
-		cout << "Invalid letter"<<endl;
+	
+	
 	system("pause");
 }
+
 
